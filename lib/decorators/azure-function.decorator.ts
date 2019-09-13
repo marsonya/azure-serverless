0500d0e416ce696baddbfdf600259290b627dbe4
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-09-13 11:52:48 +0200

    chore() publish 0.0.2 release

--- lib/decorators/azure-function.decorator.ts
+++ lib/decorators/azure-function.decorator.ts
@@ -2,6 +2,6 @@ export type AzureFunctionOptionsKeys = 'name' | 'app' | string;
 
 export function AzureFunction(
   options?: Record<AzureFunctionOptionsKeys, any>,
-): (options?: Record<AzureFunctionOptionsKeys, any>) => ClassDecorator {
-  return () => () => {};
+): ClassDecorator {
+  return () => {};
 }
